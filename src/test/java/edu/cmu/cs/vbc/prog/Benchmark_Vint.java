468ff8080ea0bbccc0d677cd09b6d757372feb47
Lukas Lazarek <LukasALazarek@gmail.com>
2017-06-09 16:40:46 -0400

    Modified benchmark program to not use arrays

--- src/test/java/edu/cmu/cs/vbc/prog/Benchmark_Vint.java
+++ src/test/java/edu/cmu/cs/vbc/prog/Benchmark_Vint.java
@@ -2,48 +2,52 @@ package edu.cmu.cs.vbc.prog;
 
 import edu.cmu.cs.varex.annotation.VConditional;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.DoubleUnaryOperator;
-import java.util.function.IntConsumer;
-
 /**
  * Created by lukas on 6/2/17.
  */
 public class Benchmark_Vint {
     @VConditional
-    static boolean use30 = true;
+    boolean use30 = true;
 
     @VConditional
-    static boolean doubleLimit = true;
+    boolean doubleLimit = true;
     
     public static void main(String[] args){
-        int[] polynomial_int;
-        if (use30) {
+        Benchmark_Vint self = new Benchmark_Vint();
+        int c1, c2, c3, c4;
+        if (self.use30) {
             // x^3 + 1850 x + 75 x^2 + 15000 = (x + 20)(x + 25)(x + 30)
-            int[] poly= {1, 75, 1850, 15000};
-            polynomial_int = poly;
+            c1 = 1;
+            c2 = 75;
+            c3 = 1850;
+            c4 = 15000;
         } else {
             // x^3 + 1805 x + 74 x^2 + 14500 = (x + 20)(x + 25)(x + 29)
-            int[] poly = {1, 74, 1805, 14500};
-            polynomial_int = poly;
+            c1 = 1;
+            c2 = 74;
+            c3 = 1805;
+            c4 = 14500;
         }
-        System.out.println(findRoots(polynomial_int));
+        self.findRoots(c1, c2, c3, c4);
     }
 
-    public static int maxCoefficient(int[] nums){
-        int maxSoFar = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            maxSoFar = nums[i] > maxSoFar ? nums[i] : maxSoFar;
+    public int maxCoefficient(int c1, int c2, int c3, int c4){
+        if (c1 > c2 && c1 > c3 && c1 > c4) {
+            return c1;
+        } else if (c2 > c1 && c2 > c3 && c2 > c4) {
+            return c2;
+        } else if (c3 > c1 && c3 > c2 && c3 > c4) {
+            return c3;
+        } else {
+            return c4;
         }
-        return maxSoFar;
     }
 
 
-    public static int pow_int(int base, int expt){
+    public int pow_int(int base, int expt){
         return pow_int(base, expt, 1);
     }
-    public static int pow_int(int base, int expt, int valueSoFar){
+    public int pow_int(int base, int expt, int valueSoFar){
         if (expt < 1){
             return valueSoFar;
         } else {
@@ -52,30 +56,25 @@ public class Benchmark_Vint {
     }
 
 
-    public static boolean isRoot(int x, int[] polynomial){
-        int sum = 0;
-        for (int i = 0; i < polynomial.length; i++) {
-            sum += polynomial[polynomial.length - i - 1]*pow_int(x, i);
-        }
-        return sum == 0;
+    public boolean isRoot(int x, int c1, int c2, int c3, int c4){
+        return (c1*pow_int(x, 3) + c2*pow_int(x, 2) + c3*x + c4) == 0;
     }
 
 
     // Assumes POLYNOMIAL's first term is the first non-zero term
     // Also assumes POLYNOMIAL's length >= 1
-    public static List<Integer> findRoots(int[] polynomial){
-        assert(polynomial.length > 0);
-	int multiplier = polynomial.length;
-	if (doubleLimit) {
-	    multiplier *= 2;
-	}
-        int limit = multiplier*maxCoefficient(polynomial)/polynomial[0];
-        List<Integer> roots = new LinkedList<>();
+    public void findRoots(int c1, int c2, int c3, int c4){
+        int multiplier = 4;
+        if (doubleLimit) {
+            multiplier *= 2;
+        }
+        int limit = multiplier*maxCoefficient(c1, c2, c3, c4)/c1;
+        System.out.print("[ ");
         for (int root = -limit; root <= limit; root++) {
-            if (isRoot(root, polynomial)){
-                roots.add(root);
+            if (isRoot(root, c1, c2, c3, c4)){
+                System.out.print(root + " ");
             }
         }
-        return roots;
+        System.out.println("]");
     }
 }
