b39acd3676c93b13eac33b0c5db859502f1adbc5
Lukas Lazarek <LukasALazarek@gmail.com>
2017-06-11 19:48:53 -0400

    ISUB -> Vint

--- src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/ArithmeticInstructions.scala
+++ src/main/scala/edu/cmu/cs/vbc/vbytecode/instructions/ArithmeticInstructions.scala
@@ -45,7 +45,7 @@ case class InstrIADD() extends BinOpInstruction {
   override def toVByteCode(mv: MethodVisitor, env: VMethodEnv, block: Block): Unit = {
     if (env.shouldLiftInstr(this)) {
       loadCurrentCtx(mv, env, block)
-      mv.visitMethodInsn(INVOKESTATIC, vopsclassname, "IADD", s"(Ledu/cmu/cs/varex/V;Ledu/cmu/cs/varex/V;$fexprclasstype)Ledu/cmu/cs/varex/V;", false)
+      mv.visitMethodInsn(INVOKESTATIC, vopsclassname, "IADD", s"(Ledu/cmu/cs/varex/Vint;Ledu/cmu/cs/varex/Vint;$fexprclasstype)Ledu/cmu/cs/varex/Vint;", false)
     }
     else
       mv.visitInsn(IADD)
