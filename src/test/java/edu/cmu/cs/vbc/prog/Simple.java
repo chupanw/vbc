d4f792856caae58fa75c7bd70f4f9a88cdafffb8
Lukas Lazarek <LukasALazarek@gmail.com>
2017-06-23 11:18:10 -0400

    char and byte fields working

--- src/test/java/edu/cmu/cs/vbc/prog/Simple.java
+++ src/test/java/edu/cmu/cs/vbc/prog/Simple.java
@@ -6,8 +6,8 @@ package edu.cmu.cs.vbc.prog;
 public class Simple {
     int f1 = 5;
     boolean f2 = false;
-//    char f3 = 'a';
-//    byte f4 = 1;
+    char f3 = 'a';
+    byte f4 = 1;
 
 //    static int sf1;
     public static void main(String[] args){
@@ -28,20 +28,20 @@ public class Simple {
         int b = s.toN(a);
         String g = s.toS(1);
         int c = s.toI("");
-//        char p = s.toC(1);
-//        int q = s.toI(p);
-//        byte r = s.toB(1);
-//        int t = s.toI(r);
+        char p = s.toC(1);
+        int q = s.toI(p);
+        byte r = s.toB(1);
+        int t = s.toI(r);
 
         // fields
         int o = s.f1;
         s.f1 = 1;
         boolean l = s.f2;
         s.f2 = true;
-//        char m = s.f3;
-//        s.f3 = 'a';
-//        byte n = s.f4;
-//        s.f4 = 5;
+        char m = s.f3;
+        s.f3 = 'a';
+        byte n = s.f4;
+        s.f4 = 5;
     }
 
     public int foo(int x) {
@@ -71,20 +71,20 @@ public class Simple {
     public int toI(String s) {
         return 1;
     }
-//
-//    public char toC(int x) {
-//        return 'a';
-//    }
-//
-//    public int toI(char c) {
-//        return 1;
-//    }
-//
-//    public byte toB(int x) {
-//        return 1;
-//    }
-//
-//    public int toI(byte b) {
-//        return 1;
-//    }
+
+    public char toC(int x) {
+        return 'a';
+    }
+
+    public int toI(char c) {
+        return 1;
+    }
+
+    public byte toB(int x) {
+        return 1;
+    }
+
+    public int toI(byte b) {
+        return 1;
+    }
 }
