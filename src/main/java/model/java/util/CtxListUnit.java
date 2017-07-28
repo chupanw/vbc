10d43c576a50be18930965eed63b8d451a457cca
Lukas Lazarek <LukasALazarek@gmail.com>
2017-07-28 16:38:59 -0400

    Fixed incorrect CtxList methods, added unit tests

--- src/main/java/model/java/util/CtxListUnit.java
+++ src/main/java/model/java/util/CtxListUnit.java
@@ -22,6 +22,9 @@ public class CtxListUnit {
         run("Simplify V", () -> test_simplify_V());
         run("Remove", () -> test_remove());
         run("Remove V", () -> test_remove_V());
+        run("RemoveIndex V", () -> test_removeIndex_V());
+        run("Get V", () -> test_get_V());
+        run("IndexOf V", () -> test_indexOf_V());
     }
 
     public static void run(String name, Runnable f) {
@@ -45,44 +48,44 @@ public class CtxListUnit {
     public static void test_add_get() {
         CtxList<Integer> list = new CtxList<>();
         list.add(1, T);
-        assertEq(list.get(0, T), 1);
-        assertEq(list.get(0, A), 1);
-        assertEq(list.get(0, B), 1);
+        assertEq(list.get(0, T), V.one(T, 1));
+        assertEq(list.get(0, A), V.one(A, 1));
+        assertEq(list.get(0, B), V.one(B, 1));
         list.add(2, T);
-        assertEq(list.get(1, T), 2);
-        assertEq(list.get(1, A), 2);
-        assertEq(list.get(1, B), 2);
+        assertEq(list.get(1, T), V.one(T, 2));
+        assertEq(list.get(1, A), V.one(A, 2));
+        assertEq(list.get(1, B), V.one(B, 2));
 
         list.add(3, A);
         list.add(4, A.not());
-        assertEq(list.get(2, A), 3);
-        assertEq(list.get(2, A.not()), 4);
+        assertEq(list.get(2, A), V.one(A, 3));
+        assertEq(list.get(2, A.not()), V.one(A.not(), 4));
 
         list.add(5, A.or(B));
-        assertEq(list.get(3, A), 5);
-        assertEq(list.get(3, A.not()), 5);
+        assertEq(list.get(3, A), V.one(A, 5));
+        assertEq(list.get(3, A.not()), V.choice(B, V.one(A.not(), 5), V.one(A.not(), null)));
     }
 
     public static void test_add_get_V() {
         CtxList<Integer> list = new CtxList<>();
-//        list.add__Ljava_lang_Object__Z(V.one(T, 1));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0)), V.one(T, 1));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 0)), V.one(A, 1));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(B, 0)), V.one(B, 1));
-//        list.add__Ljava_lang_Object__Z(V.one(T, 2));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 1)), V.one(T, 2));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 1)), V.one(A, 2));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(B, 1)), V.one(B, 2));
-//
-//        list.add__Ljava_lang_Object__Z(V.one(A, 3));
-//        list.add__Ljava_lang_Object__Z(V.one(A.not(), 4));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 2)), V.one(A, 3));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A.not(), 2)), V.one(A.not(), 4));
-//
-//        list.add__Ljava_lang_Object__Z(V.one(A.or(B), 5));
-//        assertEq(list.get(3, A), 5);
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 3)), V.one(A, 5));
-//        assertEq(list.get__I__Ljava_lang_Object(V.one(A.not(), 3)), V.one(A.not(), 5));
+        list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T), V.one(T, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 0), A), V.one(A, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(B, 0), B), V.one(B, 1));
+        list.add__Ljava_lang_Object__Z(V.one(T, 2), T);
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 1), T), V.one(T, 2));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 1), A), V.one(A, 2));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(B, 1), B), V.one(B, 2));
+
+        list.add__Ljava_lang_Object__Z(V.one(A, 3), A);
+        list.add__Ljava_lang_Object__Z(V.one(A.not(), 4), A.not());
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 2), A), V.one(A, 3));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A.not(), 2), A.not()), V.one(A.not(), 4));
+
+        list.add__Ljava_lang_Object__Z(V.one(A.or(B), 5), A.or(B));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A, 3), A), V.one(A, 5));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(A.not(), 3), A.not()),
+                V.choice(B, V.one(A.not(), 5), V.one(A.not(), null)));
     }
 
     public static void test_size() {
@@ -102,8 +105,11 @@ public class CtxListUnit {
         list.add__Ljava_lang_Object__Z(V.one(T, 2), T);
         assertEq(list.size____I(T), V.one(T, 2));
 
-        list.add__Ljava_lang_Object__Z(V.one(T, 3), T);
-        assertEq(list.size____I(T), V.one(T, 3));
+        list.add__Ljava_lang_Object__Z(V.one(A, 3), A);
+        assertEq(list.size____I(T), V.choice(A, 3, 2));
+
+        list.add__Ljava_lang_Object__Z(V.one(B, 4), B);
+        assertEq(list.size____I(T), V.choice(A, V.choice(B, 4, 3), V.choice(B, 3, 2)));
     }
 
     public static void test_simplify() {
@@ -141,6 +147,38 @@ public class CtxListUnit {
         assertEq(list.size(), 3);
     }
 
+    public static void test_get_V() {
+        CtxList<Integer> list = new CtxList<>();
+        list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
+        list.add__Ljava_lang_Object__Z(V.one(A, 2), A);
+        list.add__Ljava_lang_Object__Z(V.one(A.or(B), 3), A.or(B));
+        list.add__Ljava_lang_Object__Z(V.one(B, 4), B);
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T), V.one(T, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 1), T),
+                V.choice(A, V.one(A, 2), V.choice(B, 3, null)));
+        // fails with equivalent ctxs? Seems like the expression isn't being simplified properly.
+        // !A ^ B ^ (A v B) <=> (!A ^ B ^ A) v (!A ^ B ^ B) <=> 0 v (!A ^ B) <=> !A ^ B
+        assertEq(list.get__I__Ljava_lang_Object(V.one(B, 1), B),
+                V.choice(A, V.one(B, 2), V.one(B, 3)));
+    }
+
+    public static void test_indexOf_V() {
+        CtxList<Integer> list = new CtxList<>();
+        list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
+        list.add__Ljava_lang_Object__Z(V.one(A, 2), A);
+        list.add__Ljava_lang_Object__Z(V.one(A.or(B), 3), A.or(B));
+        list.add__Ljava_lang_Object__Z(V.one(B, 4), B);
+        list.add__Ljava_lang_Object__Z(V.one(B, 2), B);
+
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.one(T, 0));
+        // fails with equivalent ctxs? Seems like the expression isn't being simplified properly.
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 2), T),
+                V.choice(A, V.one(A, 1), V.choice(B, 3, -1)));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 3), T),
+                V.choice(A, V.one(A, 2), V.choice(B, 1, -1)));
+    }
+
 
     public static void test_remove() {
         CtxList<Integer> list = new CtxList<>();
@@ -159,17 +197,59 @@ public class CtxListUnit {
     public static void test_remove_V() {
         CtxList<Integer> list = new CtxList<>();
         list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
-        list.remove__Ljava_lang_Object__V(V.one(T, 1), T);
+        list.remove__Ljava_lang_Object__Z(V.one(T, 1), T);
         list.simplify____V();
         assertEq(list.size____I(T), V.one(T, 0));
 
         list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
-        list.remove__Ljava_lang_Object__V(V.one(A, 1), A);
+        list.remove__Ljava_lang_Object__Z(V.one(A, 1), A);
         list.simplify____V();
         assertEq(list.size____I(T), V.choice(A, 0, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T),
+                V.choice(A, null, 1));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.choice(A, -1, 0));
 
-        list.remove__Ljava_lang_Object__V(V.one(A.not(), 1), A.not());
+        list.remove__Ljava_lang_Object__Z(V.one(B, 1), B);
+        assertEq(list.size____I(T), V.choice(A.or(B), 0, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T),
+                V.choice(A.or(B), null, 1));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.choice(A.or(B), -1, 0));
+
+        list.remove__Ljava_lang_Object__Z(V.one(A.not(), 1), A.not());
         list.simplify____V();
         assertEq(list.size____I(T), V.one(T, 0));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.one(T, -1));
+    }
+    public static void test_removeIndex_V() {
+        CtxList<Integer> list = new CtxList<>();
+        list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
+        list.remove__I__Ljava_lang_Object(V.one(T, 0), T);
+        list.simplify____V();
+        assertEq(list.size____I(T), V.one(T, 0));
+
+        list.add__Ljava_lang_Object__Z(V.one(T, 1), T);
+        list.remove__I__Ljava_lang_Object(V.one(A, 0), A);
+        list.simplify____V();
+        assertEq(list.size____I(T), V.choice(A, 0, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T),
+                V.choice(A, null, 1));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.choice(A, -1, 0));
+
+        list.remove__I__Ljava_lang_Object(V.one(B, 0), B);
+        assertEq(list.size____I(T), V.choice(A.or(B), 0, 1));
+        assertEq(list.get__I__Ljava_lang_Object(V.one(T, 0), T),
+                V.choice(A.or(B), null, 1));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.choice(A.or(B), -1, 0));
+
+        list.remove__I__Ljava_lang_Object(V.one(A.not(), 0), A.not());
+        list.simplify____V();
+        assertEq(list.size____I(T), V.one(T, 0));
+        assertEq(list.indexOf__Ljava_lang_Object__I(V.one(T, 1), T),
+                V.one(T, -1));
     }
 }
