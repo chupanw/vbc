4e3b748982e0e74a3ee79bb5dee423e4a8dfb273
Lukas Lazarek <LukasALazarek@gmail.com>
2017-11-08 14:42:27 -0500

    transformLoopPredecessor test finished

--- src/test/scala/edu/cmu/cs/vbc/utils/IterationTransformerTest.scala
+++ src/test/scala/edu/cmu/cs/vbc/utils/IterationTransformerTest.scala
@@ -4,29 +4,34 @@ import edu.cmu.cs.vbc.vbytecode.instructions._
 import edu.cmu.cs.vbc.vbytecode._
 import org.objectweb.asm.tree._
 import org.objectweb.asm.util.{CheckClassAdapter, Textifier, TraceClassVisitor}
-import org.objectweb.asm.{ClassReader, ClassVisitor, ClassWriter}
+import org.objectweb.asm._
 import org.objectweb.asm.Opcodes._
 import org.scalatest.{FunSuite, Matchers}
 
 import scala.collection.JavaConversions._
-
 import PartialFunction.cond
 
+import edu.cmu.cs.vbc.utils.LiftUtils.vclassname
 
 class IterationTransformerTest extends FunSuite with Matchers {
 
-  def equal(a: CFG, b: CFG): Boolean = {
-    val sizeEqual = a.blocks.size == b.blocks.size
+  def equal(a: Block, b: Block): Boolean = {
     // No value equality for instrs by design, so this is an ugly workaround
-    val blockStrs = a.blocks.zip(b.blocks) map (el => (el._1.instr.toList.toString + "\n", el._2.instr.toList.toString + "\n"))
-    val blocksEqual = blockStrs.foldLeft(true)((collected, el) => (el._1 == el._2) && collected)
-    if (sizeEqual && blocksEqual)
+    val aStr = a.instr.toList.toString
+    val bStr = b.instr.toList.toString
+    if (aStr == bStr) {
       true
-    else {
-      print(blockStrs)
+    } else {
+      print(aStr + "\n" + bStr)
       false
     }
   }
+  def equal(a: List[Block], b: List[Block]): Boolean = {
+    val sizeEqual = a.size == b.size
+    val blocksEqual = a.zip(b).foldRight(true)((blocks: (Block, Block), matchSoFar: Boolean) => matchSoFar && equal(blocks._1, blocks._2))
+    sizeEqual && blocksEqual
+  }
+  def equal(a: CFG, b: CFG): Boolean = equal(a.blocks, b.blocks)
 
   val cfg_1loop = CFG(List(
     Block(InstrLDC("orig 1"), InstrPOP(), InstrDUP(), InstrGOTO(1)), // 0
@@ -60,10 +65,35 @@ class IterationTransformerTest extends FunSuite with Matchers {
     Block(InstrLDC("orig 9"), // 8
       InstrINVOKEINTERFACE(Owner("Iterator"), MethodName("next"), MethodDesc("()Ljava_util_object;"), true),
       InstrDUP(), InstrPOP(), InstrGOTO(9)),
-    Block(InstrLDC("orig 10"), InstrDUP(), InstrPOP(), InstrIFEQ(10)), // 9
+    Block(InstrLDC("orig 10"), InstrDUP(), InstrPOP(), InstrIFEQ(11)), // 9
     Block(InstrLDC("orig 11"), InstrDUP(), InstrPOP(), InstrGOTO(7)), // 10: loop ^
     Block(InstrLDC("orig 12"), InstrDUP(), InstrPOP(), InstrGOTO(11)) // 11
     ))
+
+//  val valid_cfg_2loop = CFG(List(
+//    Block(InstrLDC("orig 1"), InstrPOP(), InstrICONST(1), InstrPOP(), InstrRETURN())))
+  val valid_cfg_2loop = CFG(List(
+    Block(InstrLDC("orig 1"), InstrPOP(), InstrICONST(1), InstrDUP(), InstrGOTO(1)), // 0
+    Block(InstrLDC("orig 2"), InstrPOP(), InstrPOP(), InstrICONST(2), InstrGOTO(2)), // 1
+    Block(InstrLDC("orig 3"), InstrPOP(), InstrSWAP(), InstrPOP(), // 2
+      InstrINVOKEVIRTUAL(Owner("List"), MethodName("iterator"), MethodDesc("()Ljava_util_Iterator;"), true),
+      InstrPOP(), InstrGOTO(3)),
+    Block(InstrLDC("orig 4"), InstrPOP(), InstrICONST(1), InstrGOTO(4)), // 3: loop entry
+    Block(InstrLDC("orig 5"), InstrPOP(), // 4
+      InstrINVOKEINTERFACE(Owner("Iterator"), MethodName("next"), MethodDesc("()Ljava_util_object;"), true),
+      InstrDUP(), InstrPOP(), InstrGOTO(5)),
+    Block(InstrLDC("orig 6"), InstrPOP(), InstrIFEQ(3)), // 5: loop ^
+    Block(InstrLDC("orig 7"), InstrPOP(), InstrICONST(3), // 6
+      InstrINVOKEVIRTUAL(Owner("List"), MethodName("iterator"), MethodDesc("()Ljava_util_Iterator;"), true),
+      InstrPOP(), InstrGOTO(7)),
+    Block(InstrLDC("orig 8"), InstrPOP(), InstrICONST(7), InstrGOTO(8)), // 7: loop entry
+    Block(InstrLDC("orig 9"), InstrPOP(), // 8
+      InstrINVOKEINTERFACE(Owner("Iterator"), MethodName("next"), MethodDesc("()Ljava_util_object;"), true),
+      InstrDUP(), InstrPOP(), InstrGOTO(9)),
+    Block(InstrLDC("orig 10"), InstrPOP(), InstrIFEQ(11)), // 9
+    Block(InstrLDC("orig 11"), InstrPOP(), InstrICONST(8), InstrPOP(), InstrGOTO(7)), // 10: loop ^
+    Block(InstrLDC("orig 12"), InstrPOP(), InstrICONST(9), InstrPOP(), InstrRETURN()) // 11
+  ))
 
 
   // ===== insertCleanupBlocks =====
@@ -120,7 +150,7 @@ class IterationTransformerTest extends FunSuite with Matchers {
         InstrIFNE(12)),
       Block(List(InstrPOP(), InstrPOP(), InstrGOTO(9)), Nil), // 11: cleanup block
       Block(List(InstrDUP(), InstrPOP(), InstrGOTO(13)), Nil), // 12: second split-half
-      Block(InstrLDC("orig 10"), InstrDUP(), InstrPOP(), InstrIFEQ(14)), // 13
+      Block(InstrLDC("orig 10"), InstrDUP(), InstrPOP(), InstrIFEQ(15)), // 13
       Block(InstrLDC("orig 11"), InstrDUP(), InstrPOP(), InstrGOTO(9)), // 14: loop ^
       Block(InstrLDC("orig 12"), InstrDUP(), InstrPOP(), InstrGOTO(15)) // 15
     ))), "Block splitting doesn't work as expected")
@@ -162,13 +192,6 @@ class IterationTransformerTest extends FunSuite with Matchers {
     assert(blockUpdates(9) == 13)
     assert(blockUpdates(10) == 14)
     assert(blockUpdates(11) == 15)
-  }
-
-
-  // ===== transformLoopPredecessor =====
-  test("transformLoopPredecessor works") {
-    val loopPredecessor = cfg_2loop.blocks(2)
-    val env = ???
   }
 
   // ===== createSimplifyLambda =====
@@ -213,6 +236,52 @@ class IterationTransformerTest extends FunSuite with Matchers {
     // todo: figure out checking JVM compliance
 //    cr.accept(new CheckClassAdapter(cw), 0) // throws null exception
   }
+
+
+  // ===== transformLoopPredecessor =====
+  test("transformLoopPredecessor works") {
+    val className = "testclass"
+    val vbcMtdNode = VBCMethodNode(0, "test", "()V", None, List.empty, valid_cfg_2loop)
+    val vbcClazz = VBCClassNode(0, 0, className, None, "java/util/Object", List.empty, List.empty, List(vbcMtdNode))
+    val env = new VMethodEnv(vbcClazz, vbcMtdNode)
+
+    val itt = new IterationTransformer()
+    val cw = new MyClassWriter(ClassWriter.COMPUTE_FRAMES)
+
+    val loopPredecessor = valid_cfg_2loop.blocks(2)
+
+    val blockTrans = itt.transformLoopPredecessor(loopPredecessor, env, cw,
+      insn => valid_cfg_2loop.blocks.flatMap(_.instr).indexWhere(_ eq insn))
+
+    val lambdaName = "lambda$INVOKEVIRTUAL$simplifyCtxList"
+    val lambdaDesc = s"(${itt.ctxListClassType})V"
+    val consumerName = "java/util/function/Consumer"
+    val consumerType = s"L$consumerName;"
+
+    assert(blockTrans.newVars.isEmpty)
+    assert(equal(blockTrans.newBlocks, List(Block(
+      InstrLDC("orig 3"),
+      InstrPOP(),
+      InstrSWAP(),
+      InstrPOP(), // 13
+
+      InstrDUP(),
+      InstrINVOKEDYNAMIC(Owner(consumerName), MethodName("accept"), MethodDesc(s"()$consumerType"),
+        new Handle(H_INVOKESTATIC, "java/lang/invoke/LambdaMetafactory", "metafactory",
+          "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;"),
+        Type.getType("(Ljava/lang/Object;)V"),
+        new Handle(H_INVOKESTATIC, className, lambdaName, lambdaDesc),
+        Type.getType(lambdaDesc)),
+      InstrINVOKEINTERFACE(Owner(vclassname), MethodName("foreach"), MethodDesc(s"($consumerType)V"), true),
+
+      InstrINVOKEVIRTUAL(Owner("List"), MethodName("iterator"), MethodDesc("()Ljava_util_Iterator;"), true),
+      InstrPOP(),
+      InstrGOTO(3)
+    ))))
+    val iteratorIndex = 14
+    assert(blockTrans.newInsnIndeces == List(iteratorIndex, iteratorIndex + 1, iteratorIndex + 2))
+  }
+
 
 
   // ===== transformBodyStartBlock =====
