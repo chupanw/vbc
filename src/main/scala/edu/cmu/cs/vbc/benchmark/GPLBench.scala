19499bac08f08ec588a4676e5c81fa1387402dfd
chupanw <chupanw@cs.cmu.edu>
2018-03-14 13:29:27 -0400

    Add GPL quick benchmark

--- src/main/scala/edu/cmu/cs/vbc/benchmark/GPLBench.scala
+++ src/main/scala/edu/cmu/cs/vbc/benchmark/GPLBench.scala
@@ -28,3 +28,24 @@ object GPLVMInvocation extends App with VBench {
   writer.append(meanTimeInMS + " ms\n")
   writer.close()
 }
+
+object GPLVMInvocation_Model extends App with VBench {
+  var start: Double = 0.0
+  var end: Double = 0.0
+
+  val measurements = new SlidingWindown(10)
+
+  val m = getMainMethod("edu.cmu.cs.vbc.prog.gpl.Main", "gpl-model.conf", useModel = true)
+
+  while (!(measurements.isFull && measurements.cov() < 0.02)) {
+    start = System.nanoTime().toDouble
+    m.invoke(null, Array[String]())
+    end = System.nanoTime().toDouble
+    measurements.add(end - start)
+  }
+
+  val meanTimeInMS = measurements.mean() / 1000000
+  val writer = new FileWriter("gpl-model.result", true)
+  writer.append(meanTimeInMS + " ms\n")
+  writer.close()
+}
