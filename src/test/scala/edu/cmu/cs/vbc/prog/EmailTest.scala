ea988f1734c7a00d329ba6e7cd71b6deb06c8040
chupanw <chupanw@cs.cmu.edu>
2018-03-28 15:28:42 -0400

    Add another email test that uses model classes

--- src/test/scala/edu/cmu/cs/vbc/prog/EmailTest.scala
+++ src/test/scala/edu/cmu/cs/vbc/prog/EmailTest.scala
@@ -14,6 +14,10 @@ class EmailTest extends FunSuite with DiffLaunchTestInfrastructure {
     testMain(classOf[email.PL_Interface_impl], fm = featureModel, configFile = Some("email.conf"))
   }
 
+  test("Email-Model") {
+    testMain(classOf[email.PL_Interface_impl], fm = featureModel, configFile = Some("email-model.conf"), useModel = true)
+  }
+
   def featureModel(config: Map[String, Boolean]): Boolean = {
     for ((n, v) <- config) classOf[FeatureSwitches].getField(n).set(null, v)
     FeatureSwitches.valid_product()
