d004375f5dd9e68c44124b722e8bd1d9bf3bece6
Lukas Lazarek <LukasALazarek@gmail.com>
2018-04-10 20:16:00 -0400

    Extended align2 to work on lists of strings

--- src/main/scala/edu/cmu/cs/vbc/utils/SequenceAlignment.scala
+++ src/main/scala/edu/cmu/cs/vbc/utils/SequenceAlignment.scala
@@ -7,7 +7,7 @@ object SequenceAlignment {
     val NONMATCH_COST = 1
     type CostMatrix = Array[Array[Int]]
 
-    def align(seq1: String, seq2: String): (Int, List[(Char, Char, Int)]) = {
+    def align(seq1: List[String], seq2: List[String]): (Int, List[(String, String, Int)]) = {
       val cost = costMatrix(seq1, seq2)
       (cost(0)(0), alignment(seq1, seq2, cost))
     }
@@ -28,7 +28,7 @@ object SequenceAlignment {
       }
       m
     }
-    def costMatrix(seq1: String, seq2: String): CostMatrix = {
+    def costMatrix(seq1: List[String], seq2: List[String]): CostMatrix = {
       var m = emptyCostMatrix(seq1.length, seq2.length)
 
       for (i <- seq1.length - 1 to (0, -1);
@@ -38,21 +38,21 @@ object SequenceAlignment {
       }
       m
     }
-    def calc_cell(letters: (Char, Char), rightCell: Int, downCell: Int, diagCell: Int): Int = {
+    def calc_cell(letters: (String, String), rightCell: Int, downCell: Int, diagCell: Int): Int = {
       List(rightCell + 2, downCell + 2, diagCell + penalty(letters)).min
     }
-    def penalty(letters: (Char, Char)): Int = {
+    def penalty(letters: (String, String)): Int = {
       letters match {
         case (a, b) if a == b => MATCH_COST
-        case ('-', _) | (_, '-') => GAP_COST
+        case ("-", _) | (_, "-") => GAP_COST
         case _ => NONMATCH_COST
       }
     }
 
-    def alignment(seq1: String, seq2: String, cost: CostMatrix): List[(Char, Char, Int)] = {
-      val padded_seq1 = seq1 + "-"
-      val padded_seq2 = seq2 + "-"
-      var aligned = List.empty[(Char, Char, Int)]
+    def alignment(seq1: List[String], seq2: List[String], cost: CostMatrix): List[(String, String, Int)] = {
+      val padded_seq1 = seq1 :+ "-"
+      val padded_seq2 = seq2 :+ "-"
+      var aligned = List.empty[(String, String, Int)]
       var i = 0
       var j = 0
       while (i < seq1.length && j < seq2.length) {
@@ -65,11 +65,11 @@ object SequenceAlignment {
 
         aligned = aligned :+ (min_cost match {
           case `cost_right` =>
-            val res = (seq1(i), '-', GAP_COST)
+            val res = (seq1(i), "-", GAP_COST)
             i += 1
             res
           case `cost_down` =>
-            val res = ('-', seq2(j), GAP_COST)
+            val res = ("-", seq2(j), GAP_COST)
             j += 1
             res
           case `cost_diag` =>
@@ -80,23 +80,23 @@ object SequenceAlignment {
         })
       }
       while (i < seq1.length) {
-        aligned = aligned :+ (seq1(i), '-', GAP_COST)
+        aligned = aligned :+ (seq1(i), "-", GAP_COST)
         i += 1
       }
       while (j < seq2.length) {
-        aligned = aligned :+ ('-', seq2(j), GAP_COST)
+        aligned = aligned :+ ("-", seq2(j), GAP_COST)
         j += 1
       }
       aligned
     }
   }
-  def format(alignment: List[(Char, Char, Int)]): String = {
+  def format(alignment: List[(String, String, Int)]): String = {
     alignment.foldLeft("")((s, el) => s + el._1 + " " + el._2 + " " + el._3 + "\n")
   }
 
   def main(args: Array[String]): Unit = {
-    val test1 = "acacacac"
-    val test2 = "cacacaca"
+    val test1 = List("a", "c", "a", "cc", "aa", "c", "a", "c")
+    val test2 = List("c", "a", "cc", "aa", "c", "a", "c", "a")
     println(format(Align2.align(test1, test2)._2))
   }
 }
