aeceb8bfc994b1e0405ce4bb19d6733582430807
chupanw <chupanw@cs.cmu.edu>
2018-08-06 16:31:15 -0400

    Change TestLauncher to read test classes from files

--- src/main/scala/edu/cmu/cs/vbc/testutils/TestLauncher.scala
+++ src/main/scala/edu/cmu/cs/vbc/testutils/TestLauncher.scala
@@ -2,6 +2,7 @@ package edu.cmu.cs.vbc.testutils
 
 import de.fosd.typechef.featureexpr.{FeatureExpr, FeatureExprFactory}
 import edu.cmu.cs.vbc.VERuntime
+import scala.io.Source._
 
 /**
   * Launch JUnit test cases
@@ -10,11 +11,18 @@ import edu.cmu.cs.vbc.VERuntime
   */
 object TestLauncher extends App {
 
+  assume(args.length == 2, s"Wrong number of arguments: ${args.length}")
+
+  assume(args(0).endsWith("/"), s"Not a folder: $repository")
+  val repository = args(0)
+  val version = args(1)
+  val relevantTests = fromFile(args(0) + "RelevantTests/" + version + ".txt")
+
   FeatureExprFactory.setDefault(FeatureExprFactory.bdd)
 
   // turn this two into parameters
-  val testClasspath = "/Users/chupanw/Projects/Data/defects4j-math/6f/target/test-classes/"
-  val mainClasspath = "/Users/chupanw/Projects/Data/defects4j-math/6f/target/classes/"
+  val testClasspath = s"$repository$version/target/test-classes/"
+  val mainClasspath = s"$repository$version/target/classes/"
 
   val testLoader = new VBCTestClassLoader(this.getClass.getClassLoader, mainClasspath, testClasspath)
   VERuntime.classloader = Some(testLoader)
@@ -25,7 +33,9 @@ object TestLauncher extends App {
 //    testClass.runTests()
 //  }
 
-  val tests: List[String] = List(
+  val tests = relevantTests.getLines().toList.filterNot(_.startsWith("//"))
+
+//  val tests: List[String] = List(
     // problematic:
 //    "org.apache.commons.math3.analysis.integration.gauss.BaseRuleFactoryTest" // concurrency
 
@@ -102,53 +112,7 @@ object TestLauncher extends App {
     // problematic
 //    "org.apache.commons.math3.util.CombinatoricsUtilsTest"
 
-
-    // 1f
-//    "org.apache.commons.math3.analysis.interpolation.FieldHermiteInterpolatorTest",  // ok
-//    "org.apache.commons.math3.analysis.polynomials.PolynomialsUtilsTest",  // ok
-//    "org.apache.commons.math3.distribution.KolmogorovSmirnovDistributionTest",  // ok
-//    "org.apache.commons.math3.fraction.BigFractionFieldTest",  //ok
-//    "org.apache.commons.math3.fraction.BigFractionFormatTest",  //ok
-//    "org.apache.commons.math3.fraction.BigFractionTest",  //ok
-//    "org.apache.commons.math3.fraction.FractionFieldTest",  //ok
-//    "org.apache.commons.math3.fraction.FractionFormatTest",//ok
-//    "org.apache.commons.math3.fraction.FractionTest",//ok
-//    "org.apache.commons.math3.linear.ArrayFieldVectorTest",//ok
-//    "org.apache.commons.math3.linear.BlockFieldMatrixTest",//ok
-//    "org.apache.commons.math3.linear.FieldLUDecompositionTest",//ok
-//    "org.apache.commons.math3.linear.FieldLUSolverTest",  //ok
-//    "org.apache.commons.math3.linear.FieldMatrixImplTest",//ok
-//    "org.apache.commons.math3.linear.MatrixUtilsTest",//ok
-//    "org.apache.commons.math3.linear.SparseFieldMatrixTest",//ok
-//    "org.apache.commons.math3.linear.SparseFieldVectorTest",//ok
-//    "org.apache.commons.math3.ode.nonstiff.AdamsBashforthIntegratorTest",//ok
-//    "org.apache.commons.math3.ode.nonstiff.AdamsMoultonIntegratorTest",//ok
-//    "org.apache.commons.math3.ode.sampling.NordsieckStepInterpolatorTest",//ok
-//    "org.apache.commons.math3.util.OpenIntToFieldTest"//ok
-
-    // 6f
-    "org.apache.commons.math3.analysis.interpolation.SmoothingPolynomialBicubicSplineInterpolatorTest",
-    "org.apache.commons.math3.fitting.CurveFitterTest",
-    "org.apache.commons.math3.fitting.GaussianFitterTest",
-    "org.apache.commons.math3.fitting.HarmonicFitterTest",
-    "org.apache.commons.math3.fitting.PolynomialFitterTest",
-//    "org.apache.commons.math3.optim.linear.SimplexSolverTest", // method code too large
-    "org.apache.commons.math3.optim.nonlinear.scalar.MultiStartMultivariateOptimizerTest",
-    "org.apache.commons.math3.optim.nonlinear.scalar.MultivariateFunctionMappingAdapterTest",
-    "org.apache.commons.math3.optim.nonlinear.scalar.MultivariateFunctionPenaltyAdapterTest",
-    "org.apache.commons.math3.optim.nonlinear.scalar.gradient.NonLinearConjugateGradientOptimizerTest",
-//    "org.apache.commons.math3.optim.nonlinear.scalar.noderiv.BOBYQAOptimizerTest"
-//    "org.apache.commons.math3.optim.nonlinear.scalar.noderiv.CMAESOptimizerTest"
-    "org.apache.commons.math3.optim.nonlinear.scalar.noderiv.PowellOptimizerTest",
-    "org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizerMultiDirectionalTest",
-    "org.apache.commons.math3.optim.nonlinear.scalar.noderiv.SimplexOptimizerNelderMeadTest",
-    "org.apache.commons.math3.optim.nonlinear.vector.MultiStartMultivariateVectorOptimizerTest",
-    "org.apache.commons.math3.optim.nonlinear.vector.jacobian.GaussNewtonOptimizerTest",
-    "org.apache.commons.math3.optim.nonlinear.vector.jacobian.LevenbergMarquardtOptimizerTest",
-    "org.apache.commons.math3.optim.nonlinear.vector.jacobian.MinpackTest",
-    "org.apache.commons.math3.optim.univariate.BrentOptimizerTest",
-    "org.apache.commons.math3.optim.univariate.MultiStartUnivariateOptimizerTest"
-  )
+//  )
 
     tests.foreach {x =>
       val testClass = TestClass(testLoader.loadClass(x))
@@ -156,4 +120,5 @@ object TestLauncher extends App {
     }
 
   VTestStat.printToConsole()
+  VTestStat.toMarkdown(version, "org.apache.commons.math3.")
 }
