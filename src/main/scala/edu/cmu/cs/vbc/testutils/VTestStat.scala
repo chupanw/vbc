f13b2cea308b4a89b53c82264c613d6d005175d0
chupanw <chupanw@cs.cmu.edu>
2018-10-18 16:25:27 -0400

    Rewrite test results output
    After restricting interaction degree, expressions become too large
    to print.

--- src/main/scala/edu/cmu/cs/vbc/testutils/VTestStat.scala
+++ src/main/scala/edu/cmu/cs/vbc/testutils/VTestStat.scala
@@ -2,7 +2,10 @@ package edu.cmu.cs.vbc.testutils
 
 import java.io.{File, FileWriter}
 
-import de.fosd.typechef.featureexpr.{FeatureExpr, FeatureExprFactory}
+import de.fosd.typechef.featureexpr.bdd.{BDDFeatureExpr, BDDFeatureModel}
+import de.fosd.typechef.featureexpr.{FeatureExpr, FeatureExprFactory, SingleFeatureExpr}
+import edu.cmu.cs.varex.V
+import edu.cmu.cs.vbc.GlobalConfig
 
 import scala.collection.mutable
 
@@ -20,10 +23,10 @@ object VTestStat {
     println("*********************************")
     println("*          Test Report          *")
     println("*********************************")
-    classes.toList.sortWith((x, y) => x._1.compare(y._1) < 0).unzip._2.foreach(println)
+    classes.toList.sortWith((x, y) => x._1.compare(y._1) < 0).unzip._2.foreach(_.print2Console())
     skippedClasses foreach println
     val overallPassingCond = classes.values.map(_.getOverallPassingCondition).foldLeft(FeatureExprFactory.True)(_.and(_))
-    println("To pass all tests: " + overallPassingCond)
+    printOneSolution(overallPassingCond)
   }
 
   def toMarkdown(version: String, removePrefix: String): Unit = {
@@ -37,6 +40,20 @@ object VTestStat {
 
     println(s"Table generated: $version.md")
   }
+
+  /**
+    * Print one solution that has a degree lower than [[GlobalConfig.maxInteractionDegree]]
+    *
+    * We assume that [[de.fosd.typechef.featureexpr.bdd.BDDFeatureExpr.getSatisfiableAssignment()]] can give us the
+    * smallest solution.
+    */
+  def printOneSolution(fe: FeatureExpr): Unit = {
+    val hasLDSolution = fe.isSatisfiable() && !V.isDegreeTooHigh(fe)
+    if (hasLDSolution)
+      println(s"All test cases can pass if, for example, : ${V.getOneLowDegreeSolution(fe)}")
+    else
+      println(s"To pass all test cases, no solution within ${GlobalConfig.maxInteractionDegree} mutations")
+  }
 }
 
 case class VTestStatClass(c: String) {
@@ -44,11 +61,58 @@ case class VTestStatClass(c: String) {
   private var skippedMethods: mutable.Set[String] = mutable.Set()
   private var succeededMethods: mutable.Set[String] = mutable.Set()
 
+  /* logging */
   def skipMethod(m: String): Unit = skippedMethods += m
   def succeedMethod(m: String): Unit = succeededMethods += m
   def fail(m: String, ctx: FeatureExpr): Unit = failedMethods.getOrElseUpdate(m, VTestStatMethod(m)) logFailingContext ctx
-  def getOverallPassingCondition: FeatureExpr = failedMethods.values.map(_.getFailingContext.not()).foldLeft(FeatureExprFactory.True)(_.and(_))
 
+  /* report */
+  def getOverallPassingCondition(): FeatureExpr = failedMethods.values.map(_.getPassingContext).foldLeft(FeatureExprFactory.True)(_.and(_))
+
+  /**
+    * Use with caution, might be too slow.
+    */
+  def getOverallPassingConditionExcludingHigherD(): FeatureExpr = failedMethods.values.map(_.getPassingContextExcludingHighD).foldLeft(FeatureExprFactory.True)(_.and(_))
+
+  /**
+    * Print one solution that has a degree lower than [[GlobalConfig.maxInteractionDegree]]
+    *
+    * We assume that [[de.fosd.typechef.featureexpr.bdd.BDDFeatureExpr.getSatisfiableAssignment()]] can give us the
+    * smallest solution.
+    */
+  def printOneSolution(fe: FeatureExpr): Unit = {
+    val hasLDSolution = fe.isSatisfiable() && !V.isDegreeTooHigh(fe)
+    if (hasLDSolution)
+      println(s"$c can be fixed if, for example : ${V.getOneLowDegreeSolution(fe)}\n")
+    else
+      println(s"$c cannot be fixed with ${GlobalConfig.maxInteractionDegree} mutations\n")
+  }
+
+  /**
+    * Print solutions on the fly, as getting all solutions might take too long
+    *
+    * Excluding higher order interactions.
+    *   For individual test cases and test classes, output if satisfiable or not with regard to low interaction degree
+    *   For all test cases, get one lower-degree solution
+    */
+  def print2Console (): Unit = {
+    println(c)
+    println("[Failed]")
+    failedMethods.toList.sortWith((x, y) => x._1.compareTo(y._1) < 0).unzip._2.foreach(_.print2Console)
+    if (succeededMethods.exists(x => !failedMethods.contains(x))) {
+      println("[Succeeded]")
+      println(succeededMethods.filter(x => !failedMethods.contains(x)).mkString("\t", "\n\t", ""))
+    }
+    if (skippedMethods.nonEmpty) {
+      println("[Skipped]")
+      println(skippedMethods.mkString("\t", "\n\t", ""))
+    }
+    printOneSolution(getOverallPassingCondition())
+  }
+
+  /**
+    * Only used when we are not limiting the degree of interactions
+    */
   override def toString: String =
     s"""
        |$c pass if $getOverallPassingCondition
@@ -60,6 +124,9 @@ case class VTestStatClass(c: String) {
        |${skippedMethods.mkString("\t", "\n\t", "")}
      """.stripMargin
 
+  /**
+    * Same limitations as toString
+    */
   def toMarkdown(removePrefix: String): List[String] = {
     val shortenClazzName = c.substring(removePrefix.length)
     val skipped = for (m <- skippedMethods.toList) yield shortenClazzName + s".$m" + " | " + "skipped" + "|"
@@ -73,9 +140,56 @@ case class VTestStatMethod(m: String) {
   private var failingCtx: FeatureExpr = FeatureExprFactory.False
 
   def logFailingContext(fe: FeatureExpr): Unit = failingCtx = failingCtx or fe
-  def getFailingContext: FeatureExpr = failingCtx
 
-  override def toString: String = m + "  pass if  " + failingCtx.not()
+  /* reporting */
+  def getPassingContextExcludingHighD: FeatureExpr = restrictStartingContext(failingCtx.not())
+  def getPassingContext: FeatureExpr = failingCtx.not()
 
-  def toMarkdown(clz: String): String = s"$clz.$m | `${failingCtx.not()}` |"
+  /**
+    * In case there are too many mutations, we only output if there exists solutions that are
+    * low-degree interactions
+    */
+  def print2Console(): Unit= {
+    val x = failingCtx.not()
+    val msg = (if (x.isContradiction() || V.isDegreeTooHigh(x)) s" has no " else " has ") + s"solutions with degree lower than ${GlobalConfig.maxInteractionDegree}"
+//    val msg = " pass if " + failingCtx.not()
+    println("\t" + m + msg)
+  }
+
+  /**
+    * Used only if we are not limiting the degree of interactions
+    */
+  override def toString: String = m + "  pass if  " + getPassingContext
+  def toMarkdown(clz: String): String = s"$clz.$m | `${getPassingContext}` |"
+
+  type Solution = (List[SingleFeatureExpr], List[SingleFeatureExpr])
+
+  /**
+    * Rule out all solutions that have high-degree interactions.
+    *
+    * Use with caution, might be very slow.
+    * Might be faster to just append a feature model that excludes all higher order interactions
+    *
+    * @param fe FeatureExpr to be restricted
+    * @return Restricted FeatureExpr
+    */
+  def restrictStartingContext(fe: FeatureExpr): FeatureExpr = {
+    def getAllSats(x: FeatureExpr, options: Set[SingleFeatureExpr], allSat: Set[Solution]): Set[Solution] = {
+      val sat = x.getSatisfiableAssignment(BDDFeatureModel.empty, options, preferDisabledFeatures = true)
+      if (sat.isDefined) {
+        val satFE = construct(sat.get._1, sat.get._2)
+        getAllSats(x.and(satFE.not()), options, allSat + sat.get)
+      } else
+        allSat
+    }
+    def construct(enable: List[SingleFeatureExpr], disable: List[SingleFeatureExpr]): FeatureExpr = {
+      enable.foldLeft(FeatureExprFactory.True)(_ and _) and disable.map(_.not()).foldLeft(FeatureExprFactory.True)(_ and _)
+    }
+    if (FeatureExprFactory.True.equivalentTo(fe) || FeatureExprFactory.False.equivalentTo(fe)) fe
+    else {
+      val allSat: Set[Solution] = getAllSats(fe, fe.collectDistinctFeatureObjects, Set())
+      val invalidSat = allSat.filter(x => x._1.size > GlobalConfig.maxInteractionDegree)
+      invalidSat.foldLeft(fe)((soFar, next) => soFar.and(construct(next._1, next._2).not()))
+    }
+  }
 }
