904c6351d2c9edd1f6c6187adada6a82b4869274
chupanw <chupanw@cs.cmu.edu>
2018-11-16 16:44:59 -0500

    Add all median programs

--- src/main/scala/edu/cmu/cs/vbc/scripts/GenProg.scala
+++ src/main/scala/edu/cmu/cs/vbc/scripts/GenProg.scala
@@ -30,10 +30,10 @@ object GenProg extends App {
 //    ("median/1b31fa5c/000/", "introclassJava.median_1b31fa5c_000")  // fixed by GenProg
 //    ("median/1bf73a9c/000/", "introclassJava.median_1bf73a9c_000"), // filtered, no positive tests
 //    ("median/1bf73a9c/003/", "introclassJava.median_1bf73a9c_003")  // fixed by GenProg
-//    ("median/1c2bb3a4/000/", "introclassJava.median_1c2bb3a4_000")  // todo try harder
+//    ("median/1c2bb3a4/000/", "introclassJava.median_1c2bb3a4_000")  // todo try harder, memory issue
 //    ("median/2c155667/000/", "introclassJava.median_2c155667_000")  // todo try harder
 //    ("median/30074a0e/000/", "introclassJava.median_30074a0e_000"), // filtered, no positive tests
-    ("median/317aa705/000/", "introclassJava.median_317aa705_000"),
+//    ("median/317aa705/000/", "introclassJava.median_317aa705_000"), // todo scalability, too many ors
     ("median/317aa705/002/", "introclassJava.median_317aa705_002"),
     ("median/317aa705/003/", "introclassJava.median_317aa705_003")
 //    ("median/36d8008b/000/", "introclassJava.median_36d8008b_000"),
@@ -44,6 +44,43 @@ object GenProg extends App {
 //    ("median/68eb0bb0/000/", "introclassJava.median_68eb0bb0_000"),
 //    ("median/6aaeaf2f/000/", "introclassJava.median_6aaeaf2f_000"),
 //    ("median/6e464f2b/003/", "introclassJava.median_6e464f2b_003")
+    ("median/89b1a701/003/", "introclassJava.median_89b1a701_003"),
+    ("median/89b1a701/007/", "introclassJava.median_89b1a701_007"),
+    ("median/89b1a701/010/", "introclassJava.median_89b1a701_010"),
+    ("median/9013bd3b/000/", "introclassJava.median_9013bd3b_000"),
+    ("median/90834803/003/", "introclassJava.median_90834803_003"),
+    ("median/90834803/010/", "introclassJava.median_90834803_010"),
+    ("median/90834803/015/", "introclassJava.median_90834803_015"),
+    ("median/90a14c1a/000/", "introclassJava.median_90a14c1a_000"),
+    ("median/93f87bf2/010/", "introclassJava.median_93f87bf2_010"),
+    ("median/93f87bf2/012/", "introclassJava.median_93f87bf2_012"),
+    ("median/93f87bf2/015/", "introclassJava.median_93f87bf2_015"),
+    ("median/95362737/000/", "introclassJava.median_95362737_000"),
+    ("median/95362737/003/", "introclassJava.median_95362737_003"),
+    ("median/9c9308d4/003/", "introclassJava.median_9c9308d4_003"),
+    ("median/9c9308d4/007/", "introclassJava.median_9c9308d4_007"),
+    ("median/9c9308d4/012/", "introclassJava.median_9c9308d4_012"),
+    ("median/aaceaf4a/003/", "introclassJava.median_aaceaf4a_003"),
+    ("median/af81ffd4/004/", "introclassJava.median_af81ffd4_004"),
+    ("median/af81ffd4/007/", "introclassJava.median_af81ffd4_007"),
+    ("median/b6fd408d/000/", "introclassJava.median_b6fd408d_000"),
+    ("median/b6fd408d/001/", "introclassJava.median_b6fd408d_001"),
+    ("median/c716ee61/000/", "introclassJava.median_c716ee61_000"),
+    ("median/c716ee61/001/", "introclassJava.median_c716ee61_001"),
+    ("median/c716ee61/002/", "introclassJava.median_c716ee61_002"),
+    ("median/cd2d9b5b/010/", "introclassJava.median_cd2d9b5b_010"),
+    ("median/d009aa71/000/", "introclassJava.median_d009aa71_000"),
+    ("median/d120480a/000/", "introclassJava.median_d120480a_000"),
+    ("median/d2b889e1/000/", "introclassJava.median_d2b889e1_000"),
+    ("median/d43d3207/000/", "introclassJava.median_d43d3207_000"),
+    ("median/d4aae191/000/", "introclassJava.median_d4aae191_000"),
+    ("median/e9c6206d/000/", "introclassJava.median_e9c6206d_000"),
+    ("median/e9c6206d/001/", "introclassJava.median_e9c6206d_001"),
+    ("median/fcf701e8/000/", "introclassJava.median_fcf701e8_000"),
+    ("median/fcf701e8/002/", "introclassJava.median_fcf701e8_002"),
+    ("median/fcf701e8/003/", "introclassJava.median_fcf701e8_003"),
+    ("median/fe9d5fb9/000/", "introclassJava.median_fe9d5fb9_000"),
+    ("median/fe9d5fb9/002/", "introclassJava.median_fe9d5fb9_002")
   )
   val logger = LoggerFactory.getLogger("genprog")
 
