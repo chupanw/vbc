23f205f5458427261241a1cca9bc68a7c0ff09d1
chupanw <chupanw@cs.cmu.edu>
2019-01-20 16:00:47 -0500

    Monopoly setup

--- src/main/scala/edu/cmu/cs/vbc/testutils/ForkTestCase.scala
+++ src/main/scala/edu/cmu/cs/vbc/testutils/ForkTestCase.scala
@@ -19,13 +19,16 @@ class ForkTestCase(c: Class[_], mName: String) extends TestClass(c) {
     require(checkAnnotations, s"Unsupported annotation in $c")
     val m = getTestCases.find(x => x.getName.equals(mName))
     if (m.isDefined && !isSkipped(m.get)) {
-      if (!isParameterized)
+      if (!isParameterized) {
         executeOnce(None, m.get, FeatureExprFactory.True, mutable.ArrayBuffer[FeatureExpr]())
+        writeBDD(c.getName, m.get.getName)
+      }
       else
         for (x <- getParameters) {
           executeOnce(Some(x.asInstanceOf[Array[V[_]]]), m.get, FeatureExprFactory.True, mutable.ArrayBuffer[FeatureExpr]())
         }
 //      writePassingConditionToFile()
+      writeOneSolutionToFile()
     }
   }
 
@@ -36,6 +39,21 @@ class ForkTestCase(c: Class[_], mName: String) extends TestClass(c) {
     val fileName = "passingCond/" + c.getName + "." + mName + ".txt"
     val writer = new FileWriter(fileName)
     writer.write(VTestStat.classes.head._2.getOverallPassingCondition().toString())
+    writer.close()
+  }
+
+  def writeOneSolutionToFile(): Unit = {
+    val fileName = "passingCond/" + c.getName + ".txt"
+    val writer = new FileWriter(fileName, true)
+    if (VTestStat.classes(c.getName).failedMethods.contains(mName)) {
+      val stat = VTestStat.classes(c.getName).failedMethods(mName)
+      val oneSolution = stat.oneSolution(stat.failingCtx.not())
+      writer.write(mName + " " + oneSolution + "\n")
+    } else if (VTestStat.classes(c.getName).succeededMethods.contains(mName) || VTestStat.classes(c.getName).skippedMethods.contains(mName)) {
+      writer.write(mName + " succeed or skipped" + "\n")
+    } else {
+      writer.write(mName + " something went wrong" + "\n")
+    }
     writer.close()
   }
 }
--- src/main/scala/edu/cmu/cs/vbc/testutils/MonopoliTestLauncher.scala
+++ src/main/scala/edu/cmu/cs/vbc/testutils/MonopoliTestLauncher.scala
@@ -13,7 +13,7 @@ object MonopoliTestLoader {
     val main = "/Users/chupanw/Projects/Data/Monopooly/bin/"
     val test = "/Users/chupanw/Projects/Data/Monopooly/bin/"
 
-  val testLoader = new VBCTestClassLoader(this.getClass.getClassLoader, main, test, config = Some("monopoli.conf"), useModel = true, reuseLifted = true)
+  val testLoader = new VBCTestClassLoader(this.getClass.getClassLoader, main, test, config = Some("monopoli.conf"), useModel = true, reuseLifted = false)
 }
 
 object MonopoliTestLauncher extends App {
@@ -22,14 +22,14 @@ object MonopoliTestLauncher extends App {
   VERuntime.classloader = Some(MonopoliTestLoader.testLoader)
 
   val tests = List(
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsMatarCubiertos"
+    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsMatarCubiertos"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTest"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida1"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestPartida3"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.DadosTest"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosYo"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosMuchasTarjetas"
-    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.grupos.GrupoATests"
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.grupos.GrupoATests"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida2"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsParaCubrirMutantes"
 //    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestCubrirTodos"
@@ -93,24 +93,24 @@ object MonopoliForkTestCaseLauncher extends App {
 object MonopoliForkTestLauncher extends App {
   import scala.sys.process._
   val testClasses = List(
-    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsMatarCubiertos"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTest"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida1"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestPartida3"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.DadosTest"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosYo"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosMuchasTarjetas"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.grupos.GrupoATests"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida2"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsParaCubrirMutantes"
-//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestCubrirTodos"
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsMatarCubiertos"
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida1",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestPartida3",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.DadosTest",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosYo",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestEjerciciosMuchasTarjetas",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.grupos.GrupoATests",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTestPartida2",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestsParaCubrirMutantes",
+//    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TestCubrirTodos",
+    "edu.uclm.esi.iso5.juegos.monopoly.dominio.tests.TableroTest"
   )
   val jarFile = "out/artifacts/all_jar/vbc.jar"
 
   testClasses.foreach(c => {
     val clazz = new TestClass(MonopoliTestLoader.testLoader.loadClass(c))
     clazz.getTestCases.foreach(m => {
-      Process(Seq("java", "-cp", jarFile, "edu.cmu.cs.vbc.testutils.MonopoliForkTestCaseLauncher", c, m.getName)).lineStream.foreach(println)
+      Process(Seq("java", "-Xmx12g", "-cp", jarFile, "edu.cmu.cs.vbc.testutils.MonopoliForkTestCaseLauncher", c, m.getName)).lineStream.foreach(println)
     })
   })
 
