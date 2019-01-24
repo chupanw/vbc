51900e036c5baaf633a88621295472eea406f8a2
chupanw <chupanw@cs.cmu.edu>
2019-01-24 14:24:01 -0500

    Setup for triangle

--- src/main/scala/edu/cmu/cs/vbc/testutils/TriangleTestLauncher.scala
+++ src/main/scala/edu/cmu/cs/vbc/testutils/TriangleTestLauncher.scala
@@ -5,12 +5,13 @@ import edu.cmu.cs.vbc.VERuntime
 
 object TriangleTestLauncher extends App {
   FeatureExprFactory.setDefault(FeatureExprFactory.bdd)
+  VERuntime.classloader = Some(testLoader)
+  VERuntime.loadFeatures("triangle.txt")
 
   val triangleMain = "/Users/chupanw/Projects/vbc/target/scala-2.11/test-classes/"
   val triangleTest = "/Users/chupanw/Projects/vbc/target/scala-2.11/test-classes/"
 
-  val testLoader = new VBCTestClassLoader(this.getClass.getClassLoader, triangleMain, triangleTest, useModel = true)
-  VERuntime.classloader = Some(testLoader)
+  val testLoader = new VBCTestClassLoader(this.getClass.getClassLoader, triangleMain, triangleTest, useModel = true, reuseLifted = true)
 
   val tests = List("edu.cmu.cs.vbc.prog.triangle.Triangle_ESTest_improved")
 
