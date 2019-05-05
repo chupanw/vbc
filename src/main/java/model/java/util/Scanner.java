a8b36aa0989a0b5177b65dab086f0cdfbf6c17dc
chupanw <chupanw@cs.cmu.edu>
2019-05-04 22:39:37 -0400

    Add model for Scanner.nextDouble

--- src/main/java/model/java/util/Scanner.java
+++ src/main/java/model/java/util/Scanner.java
@@ -3,6 +3,8 @@ package model.java.util;
 import de.fosd.typechef.featureexpr.FeatureExpr;
 import edu.cmu.cs.varex.V;
 
+import java.lang.reflect.Field;
+
 public class Scanner {
     private V<? extends String> vActual;
 
@@ -14,14 +16,39 @@ public class Scanner {
         V<? extends MyPair> pairs = vActual.smap(ctx, (fe, actual) -> {
             java.util.Scanner scanner = new java.util.Scanner(actual);
             String ret = String.valueOf(scanner.nextInt());
-            int index = actual.indexOf(ret);
-            String res = actual.substring(index + ret.length());
+            int index = readPositionField(scanner);
+            String res = actual.substring(index);
             return new MyPair(ret, res);
         });
         V<? extends String> res = pairs.smap(ctx, (fe, pair) -> pair.res);
         vActual = V.choice(ctx, res, vActual);
         V<? extends Integer> ret = pairs.smap(ctx, (fe, pair) -> Integer.valueOf(pair.ret));
         return ret;
+    }
+
+    public V<? extends Double> nextDouble____D(FeatureExpr ctx) {
+        V<? extends MyPair> pairs = vActual.smap(ctx, (fe, actual) -> {
+            java.util.Scanner scanner = new java.util.Scanner(actual);
+            String ret = String.valueOf(scanner.nextDouble());
+            int index = readPositionField(scanner);
+            String res = actual.substring(index);
+            return new MyPair(ret, res);
+        });
+        V<? extends String> res = pairs.smap(ctx, (fe, pair) -> pair.res);
+        vActual = V.choice(ctx, res, vActual);
+        V<? extends Double> ret = pairs.smap(ctx, (fe, pair) -> Double.valueOf(pair.ret));
+        return ret;
+    }
+
+    private int readPositionField(java.util.Scanner scanner) {
+        try {
+            Field field = scanner.getClass().getDeclaredField("position");
+            field.setAccessible(true);
+            return (int) field.get(scanner);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     private void split(FeatureExpr ctx) {
