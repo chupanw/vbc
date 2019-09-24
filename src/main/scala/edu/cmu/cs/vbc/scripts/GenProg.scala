0fe8cecf2dfe46a29df438ff25a59e32b1bece81
chupanw <chupanw@cs.cmu.edu>
2019-09-23 22:52:26 -0400

    Update baseline GenProg results

--- src/main/scala/edu/cmu/cs/vbc/scripts/GenProg.scala
+++ src/main/scala/edu/cmu/cs/vbc/scripts/GenProg.scala
@@ -13,8 +13,8 @@ object GenProg extends App {
     * Tuples of project path (e.g., median/0cea42f9/003/) and name of the main class
     */
   val programs: List[String] = List(
-    //    "median/0cea42f9/003/"  // fixed
-    //    "median/0cdfa335/003/"  // fixed
+        "median/0cea42f9/003/",  // fixed
+        "median/0cdfa335/003/",  // fixed
     //    "median/15cb07a7/003/"  // filtered, no positive tests
     //    "median/1b31fa5c/000/"  // fixed by GenProg
     //    "median/1bf73a9c/000/", // filtered, no positive tests
@@ -28,48 +28,48 @@ object GenProg extends App {
     //    "median/c716ee61/000/", // no pos test
     //    "median/c716ee61/001/", // no pos test
     //    "median/fcf701e8/000/", // no pos test
-    //    "median/1c2bb3a4/000/",
-    "median/2c155667/000/" // cannot fix
-    //    "median/317aa705/000/", // todo: something wrong
-    //    "median/317aa705/002/",
-    //    "median/317aa705/003/",
-    //    "median/36d8008b/000/",
-    //    "median/3b2376ab/003/",
-    //    "median/3b2376ab/006/",
-    //    "median/3cf6d33a/007/",
-    //    "median/48b82975/000/",
-    //    "median/6aaeaf2f/000/",
-    //    "median/6e464f2b/003/",
-    //    "median/89b1a701/003/",
-    //    "median/89b1a701/007/",
-    //    "median/89b1a701/010/",
-    //    "median/90834803/010/",
-    //    "median/90834803/015/",
-    //    "median/90a14c1a/000/",
-    //    "median/93f87bf2/010/",
-    //    "median/93f87bf2/012/",
-    //    "median/93f87bf2/015/",
-    //    "median/9c9308d4/003/",
-    //    "median/9c9308d4/007/",
-    //    "median/9c9308d4/012/",
-    //    "median/aaceaf4a/003/",
-    //    "median/af81ffd4/004/",
-    //    "median/af81ffd4/007/",
-    //    "median/b6fd408d/000/",
-    //    "median/b6fd408d/001/",
-    //    "median/c716ee61/002/",
-    //    "median/cd2d9b5b/010/",
-    //    "median/d009aa71/000/",
-    //    "median/d120480a/000/",
-    //    "median/d2b889e1/000/",
-    //    "median/d43d3207/000/",
-    //    "median/d4aae191/000/",
-    //    "median/e9c6206d/000/",
-    //    "median/e9c6206d/001/",
-    //    "median/fcf701e8/002/",
-    //    "median/fcf701e8/003/",
-    //    "median/fe9d5fb9/000/",
-    //    "median/fe9d5fb9/002/"
+        "median/1c2bb3a4/000/",
+    "median/2c155667/000/", // cannot fix
+        "median/317aa705/000/", // todo: something wrong
+        "median/317aa705/002/",
+        "median/317aa705/003/",
+        "median/36d8008b/000/",
+        "median/3b2376ab/003/",
+        "median/3b2376ab/006/",
+        "median/3cf6d33a/007/",
+        "median/48b82975/000/",
+        "median/6aaeaf2f/000/",
+        "median/6e464f2b/003/",
+        "median/89b1a701/003/",
+        "median/89b1a701/007/",
+        "median/89b1a701/010/",
+        "median/90834803/010/",
+        "median/90834803/015/",
+        "median/90a14c1a/000/",
+        "median/93f87bf2/010/",
+        "median/93f87bf2/012/",
+        "median/93f87bf2/015/",
+        "median/9c9308d4/003/",
+        "median/9c9308d4/007/",
+        "median/9c9308d4/012/",
+        "median/aaceaf4a/003/",
+        "median/af81ffd4/004/",
+        "median/af81ffd4/007/",
+        "median/b6fd408d/000/",
+        "median/b6fd408d/001/",
+        "median/c716ee61/002/",
+        "median/cd2d9b5b/010/",
+        "median/d009aa71/000/",
+        "median/d120480a/000/",
+        "median/d2b889e1/000/",
+        "median/d43d3207/000/",
+        "median/d4aae191/000/",
+        "median/e9c6206d/000/",
+        "median/e9c6206d/001/",
+        "median/fcf701e8/002/",
+        "median/fcf701e8/003/",
+        "median/fe9d5fb9/000/",
+        "median/fe9d5fb9/002/"
   )
   val logger = LoggerFactory.getLogger("genprog")
 
@@ -97,8 +97,13 @@ object GenProg extends App {
     assert(!serCache.exists() || serCache.delete() == true)
     val jar = baseDir + "/old/uber-GenProg4Java-0.0.1-SNAPSHOT.jar"
     val jvmOps = s"-ea -Dlog4j.configuration=/$osBase/chupanw/Projects/genprog4java/src/log4j.properties"
-    val output = s"java $jvmOps -jar $jar $tmpConfigPath".!!
-    output contains "Repair Found:"
+    var found = false
+    s"java $jvmOps -jar $jar $tmpConfigPath".lineStream.foreach(x => {
+      if (x contains "Repair Found:") found = true
+      println(x)
+    })
+    if (found) logger.info("Repair found, terminating")
+    found
   }
 
   def generateGenProgConfigFile(project: String, mainClass: String, seed: Long): Unit = {
@@ -120,7 +125,7 @@ object GenProg extends App {
          |testGranularity = method
          |targetClassName = $mainClass
          |sourceVersion=1.8
-         |generations=100
+         |generations=10
          |edits = append;delete;replace;expadd;exprem;exprep;boundswitch,5.0;
          |regenPaths = true
       """.stripMargin
